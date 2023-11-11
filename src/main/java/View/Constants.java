8e71a6be95aa77db77db67639b2d25fcb9fb174b
coli-geonwoo <kkwoo001021@naver.com>
2023-11-12 05:17:29 +0900

    refactor : Constants 클래스 이동

---
+++ src/main/java/View/Constants.java
@@ -0,0 +1,13 @@
+package View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Constants {
+    public static final int CHRISTMAS =25;
+    public static final int [] WEEKEND_NUM_LIST ={1,2};
+    public static final int  [] WEEKDAY_NUM_LIST={0,3,4,5,6};
+    public static final int STARDAY_NUM=3;
+
+}
