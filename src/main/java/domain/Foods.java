58fa04df2c9b21bb57df5fa05cb4dd63463e94b9
coli-geonwoo <kkwoo001021@naver.com>
2023-11-12 03:54:40 +0900

    feat : food 리스트 객체 추가

--- src/main/java/domain/Foods.java
+++ src/main/java/domain/Foods.java
@@ -1,8 +1,24 @@
 package domain;
 
-public class Foods {
-    //
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    //중복되는 메뉴가 있는지 체크하는 기능
+
+public class Foods {
+    private List<Food> orderedFoods = new ArrayList<>();
+
+    public void appendFood(Food food){
+        checkDuplicateMenu(food.getName());
+        orderedFoods.add(food);
+    }
+
+    private void checkDuplicateMenu(String name){
+        List<String> names= orderedFoods.stream().map(Food::getName).collect(Collectors.toList());
+
+        if (names.contains(name)){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
 
 }
