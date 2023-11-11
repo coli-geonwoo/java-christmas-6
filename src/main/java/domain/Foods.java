250581eb161b243c3d5dd3eb8e718742a25c135a
coli-geonwoo <kkwoo001021@naver.com>
2023-11-12 04:04:44 +0900

    feat : 인스턴스 변수 및 할인가능 카테고리 카운팅 기능 추가

--- src/main/java/domain/Foods.java
+++ src/main/java/domain/Foods.java
@@ -7,12 +7,27 @@ import java.util.stream.Collectors;
 
 public class Foods {
     private List<Food> orderedFoods = new ArrayList<>();
+    private int mainCnt;
+    private int dessertCnt;
+    private int totalFoodCnt;
+    private int totalFoodPrice;
 
     public void appendFood(Food food){
         checkDuplicateMenu(food.getName());
+        cntCategory(food);
+        totalFoodPrice+=food.getPrice()*food.getCnt();
+        totalFoodCnt+= food.getCnt();
         orderedFoods.add(food);
     }
 
+    private void cntCategory(Food food){
+        if(food.getCategory().equals("MAIN")){
+            mainCnt+=1;
+        }
+        if(food.getCategory().equals("DESSERT")){
+            dessertCnt+=1;
+        }
+    }
     private void checkDuplicateMenu(String name){
         List<String> names= orderedFoods.stream().map(Food::getName).collect(Collectors.toList());
 
