c8820dc6a179938cd616920bb4adb0ad356a44aa
coli-geonwoo <kkwoo001021@naver.com>
2023-11-12 03:25:30 +0900

    feat : CntRange 검증 메소드 추가

--- src/main/java/View/NumValidator.java
+++ src/main/java/View/NumValidator.java
@@ -5,6 +5,9 @@ public class NumValidator {
     private static final String UNVALID_DATE_MESSAGE= "[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요";
     private static final int MONTH_START_DATE=1;
     private static final int MONTH_END_DATE=31;
+
+    private static final int minCnt=1;
+
     public static int checkIsNumber(String input){
         try{
             return Integer.parseInt(input);
@@ -14,8 +17,14 @@ public class NumValidator {
         }
     }
 
-    public static void checkRange(int input){
+    public static void checkDateRange(int input){
         if (input <MONTH_START_DATE || input>MONTH_END_DATE){
+            throw new IllegalArgumentException(UNVALID_DATE_MESSAGE);
+        }
+    }
+
+    public static void checkCntRange(int input){
+        if(input<minCnt){
             throw new IllegalArgumentException(UNVALID_DATE_MESSAGE);
         }
     }
